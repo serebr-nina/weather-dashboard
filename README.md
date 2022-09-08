bbe6e6cbb44c8ba97c87e1faf25c61217628bce3
NinaSerebryakova <serebr.nina@gmail.com>
2022-09-08 14:44:58 -0400

    added comments, edited README file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Weather Dashboard
+This application lets user find a weather forecast for a current day and four more days by entering a city name. If several cities exist with the same name, a dropdown menu for choosing a city among a list of cities with the same name, but different states, or countries appears. City search results can be saved or cleared as needed. The app displays maximum and minimum temperatures for the day, along with  the wind speed, the humidity level, the UV index, and an icon showing how sunny or cloudy the day is forecasted to be.
+
+## Screenshot
+![Weather Dashboard](./assets/image/weather-dashboard.png)
+
+## Built With
+* HTML
+* CSS
+* Javascript
+* jQuery
+
+## Purpose
+While working on this project I learned to create API keys, make serevr-side API calls, find and use the data needed for displaying in the app using Javascript and jQuery. 
+
+## Website
+
+
+## Contribution
+I used one of weather apps that I found on the GitHub website (https://jjcourtney.github.io/weather-dashboard/) as a base code. I greatly improved it's functionality while leaving CSS styles mostly unchanged besides making it more responsive for smaller screen sizes, changing colors and font.
+
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,34 @@
+// get cities by name
 const apiKey = "20d6807795f72c95b186d3f823cfbefa";
+// get weather by location
 const apiKey2 = "95075225a87b4115987247127085ca3c";
 const mainEl = $("#main")
-
 let cityFullPath = "";
-
+let cityName = '';
 // elements required
 const searchBtn = $("#search-btn");
 const searchDiv = $("#search");
 const selectorDiv = $("#selector");
 const prevSearchDiv = $("#previous-searches");
-
 const populatePreviouslySearched = () => {
     let prevCitiesArr = getCitiesFromLocalStorage();
     for (i in prevCitiesArr) {
-        console.log('here1: ' + JSON.stringify(prevCitiesArr[i]));
         const currentCityObj = {
-        "cityLat" : prevCitiesArr[i].cityLat,
-        "cityLon" : prevCitiesArr[i].cityLon,
-        "cityName" : prevCitiesArr[i].cityName,
-        "cityCountry" : prevCitiesArr[i].cityCountry,
-        "cityId" : prevCitiesArr[i].cityId,
-        "cityState" : prevCitiesArr[i].cityState
+            "cityLat": prevCitiesArr[i].cityLat,
+            "cityLon": prevCitiesArr[i].cityLon,
+            "cityName": prevCitiesArr[i].cityName,
+            "cityCountry": prevCitiesArr[i].cityCountry,
+            "cityId": prevCitiesArr[i].cityId,
+            "cityState": prevCitiesArr[i].cityState
         };
-    
         addToPreviouslySearched(currentCityObj);
     }
 }
-
 const clearPreviouslySearched = () => {
-    console.log('clear');
     window.localStorage.setItem("prevCities", "[]");
     $("#previous-searches").text("");
-    console.log('clear');
     window.localStorage.removeItem('lastCity');
 }
-
 const addToPreviouslySearched = currentCityObj => {
     const cityLon = currentCityObj.cityLon;
     const cityLat = currentCityObj.cityLat;
@@ -42,32 +36,27 @@ const addToPreviouslySearched = currentCityObj => {
     const cityCountry = currentCityObj.cityCountry;
     const cityId = currentCityObj.cityId;
     const cityState = currentCityObj.cityState;
-    console.log('cityState: ' + cityState);
     let cityFullPath = cityName;
-    // Handle optional city state
+    // handle optional city state
     if (cityState && cityState != 'undefined') {
         cityFullPath += ', ' + cityState;
     }
     cityFullPath += ', ' + cityCountry;
-    console.log('cityid ' + cityId);
-
-    console.log('search: ' + cityName + ', ' + cityId);
     prevSearchDiv.prepend($(`<span>${cityFullPath}</span>`)
-    .attr("data-city-lon", `${cityLon}`)
-    .attr("data-city-lat", `${cityLat}`)
-    .attr("data-city-name", `${cityName}`)
-    .attr("data-city-country", `${cityCountry}`)
-    .attr("data-city-id", `${cityId}`)
-    .attr("data-city-state", `${cityState}`)
-    .attr("class", "prev-searched")
-    .on("click", loadNewData));
+        .attr("data-city-lon", `${cityLon}`)
+        .attr("data-city-lat", `${cityLat}`)
+        .attr("data-city-name", `${cityName}`)
+        .attr("data-city-country", `${cityCountry}`)
+        .attr("data-city-id", `${cityId}`)
+        .attr("data-city-state", `${cityState}`)
+        .attr("class", "prev-searched")
+        .on("click", loadNewData));
 }
-
 // return true if city already added to local storage
 function isCityAlreadyinLocalStorage(city) {
     let prevCitiesArr = getCitiesFromLocalStorage();
     var found = false;
-    for (i in prevCitiesArr){
+    for (i in prevCitiesArr) {
         if (prevCitiesArr[i].cityId === city.cityId) {
             found = true;
             break;
@@ -75,132 +64,77 @@ function isCityAlreadyinLocalStorage(city) {
     }
     return found;
 }
-
 const addToLocalStorage = cityToAdd => {
-
     let prevCitiesArr = getCitiesFromLocalStorage();
-    // only add if city is not already in the list
-
     prevCitiesArr.push(cityToAdd);
     var prevCitiesStr = JSON.stringify(prevCitiesArr);
     window.localStorage.setItem("prevCities", prevCitiesStr);
-    
-    console.log('citytoadd: ' + JSON.stringify(cityToAdd));
+    // save last city viewed to default to it on revisit to the app
     window.localStorage.setItem('lastCity', JSON.stringify(cityToAdd));
-    console.log('city add: ' + cityToAdd + ', array: ' + prevCitiesArr);
 }
-
-// Retrives previous city data from local storage, sets to empty array is nothing in local storage
+// retrive previous city data from local storage, set to empty array if nothing in local storage
 const getCitiesFromLocalStorage = () => {
     prevCitiesArr = JSON.parse(window.localStorage.getItem("prevCities"));
-    if(!prevCitiesArr){
+    if (!prevCitiesArr) {
         prevCitiesArr = [];
     }
     return prevCitiesArr;
-
 }
-
 const showFoundCities = () => {
     selectorDiv.removeClass("selector-hidden").addClass("selector-visible");
-    $("#search").css({'display': 'none'});
+    $("#search").css({ 'display': 'none' });
 }
-    
 const clearAndHideFoundCities = () => {
     selectorDiv.empty();
     selectorDiv.removeClass("selector-visible").addClass("selector-hidden");
-    $("#search").css({'display': 'flex'});
+    $("#search").css({ 'display': 'flex' });
     $("#search-btn").siblings("input")[0].value = "";
 }
-
 const getEndPoint = (cityLat, cityLon) => {
     let endpoint = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityLat}&lon=${cityLon}&key=${apiKey2}&days=5&units=I`;
-    console.log('endpoint ' + endpoint);
     return endpoint;
 }
-
 const getEndPointCity = (cityName) => {
-    let endpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+    let endpoint = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
     return endpoint;
 }
-
 // load new data by getting from new endpoint
 const loadNewData = event => {
     const curCityLon = $(event.target).data("city-lon");
     const curCityLat = $(event.target).data("city-lat");
     const curCityName = $(event.target).data("city-name");
-    console.log('lon: ' + curCityLon + ', lat: ' + curCityLat + ', name: ' + curCityName);
-    //cityName = curCityName;
     const curCityCountry = $(event.target).data("city-country");
     const curCityId = $(event.target).data("city-id");
     const curCityState = $(event.target).data("city-state");
     cityFullPath = curCityName;
-    // Handle optional city state
+    // handle optional city state
     if (curCityState && curCityState != 'undefined') {
         cityFullPath += ', ' + curCityState;
     }
     cityFullPath += ', ' + curCityCountry;
-
     getDataThenPopulatePage(getEndPoint(curCityLat, curCityLon));
     $('#weather-display').show();
-    
     clearAndHideFoundCities();
     currentCityObj = {
-    "cityLat" : curCityLat,
-    "cityLon" : curCityLon,
-    "cityName" : curCityName,
-    "cityCountry" : curCityCountry,
-    "cityState": curCityState,
-    "cityId" : curCityId 
+        "cityLat": curCityLat,
+        "cityLon": curCityLon,
+        "cityName": curCityName,
+        "cityCountry": curCityCountry,
+        "cityState": curCityState,
+        "cityId": curCityId
     };
-
     if (!isCityAlreadyinLocalStorage(currentCityObj)) {
         addToLocalStorage(currentCityObj);
         addToPreviouslySearched(currentCityObj);
     }
 }
-
-// Update main card
+// update main card with saved weather data for a specific date
 const updateMainDate = currIndex => {
-    //console.log('update main date: ' + event.target.value);
-    //const index = $(event.target).data("fiveday");
-    console.log('update main index: ' + currIndex);
     const data = JSON.parse(window.localStorage.getItem("weatherData"));
     const currData = data[currIndex];
     updateToday(currData);
-    /*const curCityLat = $(event.target).data("city-lat");
-    const curCityName = $(event.target).data("city-name");
-    console.log('lon: ' + curCityLon + ', lat: ' + curCityLat + ', name: ' + curCityName);
-    //cityName = curCityName;
-    const curCityCountry = $(event.target).data("city-country");
-    const curCityId = $(event.target).data("city-id");
-    const curCityState = $(event.target).data("city-state");
-    cityFullPath = curCityName;
-    // Handle optional city state
-    if (curCityState && curCityState != 'undefined') {
-        cityFullPath += ', ' + curCityState;
-    }
-    cityFullPath += ', ' + curCityCountry;
-
-    getDataThenPopulatePage(getEndPoint(curCityLat, curCityLon));
-    $('#weather-display').show();
-    
-    clearAndHideFoundCities();
-    currentCityObj = {
-    "cityLat" : curCityLat,
-    "cityLon" : curCityLon,
-    "cityName" : curCityName,
-    "cityCountry" : curCityCountry,
-    "cityState": curCityState,
-    "cityId" : curCityId 
-    };
-
-    if (!isCityAlreadyinLocalStorage(currentCityObj)) {
-        addToLocalStorage(currentCityObj);
-        addToPreviouslySearched(currentCityObj);
-    }*/
 }
-
-// displays cities in a div and makes them selectable
+// display cities in a div and make them selectable
 const showCitiesFound = currentCity => {
     const cityId = '' + currentCity.lon + '' + currentCity.lat;
     const cityLon = currentCity.lon;
@@ -209,175 +143,141 @@ const showCitiesFound = currentCity => {
     const cityCountry = currentCity.country;
     const cityState = currentCity.state;
     let cityFullPath = cityName;
-    // Handle optional city state
+    // handle optional city state
     if (cityState != null) {
         cityFullPath += ', ' + cityState;
     }
     cityFullPath += ', ' + cityCountry;
-
     showFoundCities();
-
     selectorDiv.append($(`<span>${cityFullPath}</span><br>`)
-    .attr("data-city-lon", `${cityLon}`)
-    .attr("data-city-lat", `${cityLat}`)
-    .attr("data-city-name", `${cityName}`)
-    .attr("data-city-country", `${cityCountry}`)
-    .attr("data-city-id", `${cityId}`)
-    .attr("data-city-state", `${cityState}`)
-    .on("click", loadNewData));
-
+        .attr("data-city-lon", `${cityLon}`)
+        .attr("data-city-lat", `${cityLat}`)
+        .attr("data-city-name", `${cityName}`)
+        .attr("data-city-country", `${cityCountry}`)
+        .attr("data-city-id", `${cityId}`)
+        .attr("data-city-state", `${cityState}`)
+        .on("click", loadNewData));
 }
-
-// uses cities object to find matching cities
+// use cities object to find matching cities
 const searchCity = (arrCity) => {
-        console.log('cities' + arrCity);
-        selectorDiv.empty();   
-        if (arrCity.length > 0 && cityName != ""){
-            for (i in arrCity){
-                console.log('city string ' + JSON.stringify(arrCity[i]));
-                showCitiesFound(arrCity[i]);
-            }
-        }else{
-            showFoundCities();
-
-            selectorDiv.append($(`<p>City not found, Please search again</p>`));
-            selectorDiv.on("click", clearAndHideFoundCities);
+    selectorDiv.empty();
+    if (arrCity.length > 0 && cityName != "") {
+        for (i in arrCity) {
+            showCitiesFound(arrCity[i]);
         }
-}     
-
-// returns string reprenting the month from interger
+    } else {
+        showFoundCities();
+        selectorDiv.append($(`<p>City not found, Please search again</p>`));
+        selectorDiv.on("click", clearAndHideFoundCities);
+    }
+}
+// return string printing the month from integer
 const intToMonth = monthAsInt => {
-    const month= ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     return month[monthAsInt];
 }
-// returns string reprenting the day from interger
+// return string printing the day from integer
 const intToDay = dayAsInt => {
-    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     return weekday[dayAsInt];
 }
-
-// updates 5 day forecast cards with data
+// update 5 day forecast cards with data
 const updateFiveDay = daysData => {
-    console.log('five day ' + daysData);
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
         const currentDiv = $(`div[data-fiveday="${i}"]`)[0];
         const currentIcon = daysData[i].weather.icon;
+        //  get dynamically icon from the weatherbit website
         const currentIconSrc = `https://www.weatherbit.io/static/img/icons/${currentIcon}.png`;
         const d = new Date(daysData[i].ts * 1000);
-        console.log('date ' + d + ' ' + daysData[i].datetime);
         const day = intToDay(d.getDay());
-        currentDiv.setAttribute("onclick","updateMainDate('" + i + "')");
-        //console.log('daily card ' + JSON.stringify(currentDiv));
+        currentDiv.setAttribute("onclick", "updateMainDate('" + i + "')");
         $(currentDiv).children(".max")[0].textContent = `Max: ${daysData[i].high_temp}°F`
         $(currentDiv).children(".min")[0].textContent = `Min: ${daysData[i].low_temp}°F`
         $(currentDiv).children(".wind")[0].textContent = `Wind: ${daysData[i].wind_spd}mph`
         $(currentDiv).children(".humidity")[0].textContent = `Humidity: ${daysData[i].rh}%`
         $(currentDiv).children(".weather-img")[0].src = currentIconSrc;
         $(currentDiv).children(".day")[0].textContent = day;
-        //$(currentDiv).on("click", updateMainDate);
-        // store weather data as attributes for updating main card when clicked
-        /*.attr("data-max", `${daysData[i].high_temp}`)
-        .attr("data-min", `${daysData[i].low_temp}`)
-        .attr("data-wind", `${daysData[i].wind_spd}`)
-        .attr("data-rh", `${daysData[i].rh}`)
-        .attr("data-day", `${day}`)
-        .attr("data-uv", `${daysData[i].uv}`);*/
     }
 }
-
-// updates todays forecast card
+// update todays forecast card
 const updateToday = todayData => {
     const currentIcon = todayData.weather.icon;
-    const currentIconSrc = `https://www.weatherbit.io/static/img/icons/${currentIcon}.png`;          
+    const currentIconSrc = `https://www.weatherbit.io/static/img/icons/${currentIcon}.png`;
     const uv = todayData.uv;
-
-    // Getting today as a day, DoM and month
-    const d = new Date(todayData.ts * 1000);   
+    // get today as a day, DoM, and month
+    const d = new Date(todayData.ts * 1000);
     const day = intToDay(d.getDay());
     const dayMonth = d.getDate();
     const month = intToMonth(d.getMonth());
-
-    // Populating all todays information
+    // populate all todays information
     $("#today-max")[0].textContent = `Max: ${todayData.high_temp}°F`;
     $("#today-min")[0].textContent = `Min: ${todayData.low_temp}°F`;
     $("#today-wind")[0].textContent = `Wind: ${todayData.wind_spd}mph`;
     $("#today-humidity")[0].textContent = `Humidity: ${todayData.rh}%`;
     $("#today-img")[0].src = currentIconSrc;
-    //console.log('city name ' + todayData.city_name);
     $("#city-name")[0].textContent = `${cityFullPath}`;
     $("#today-uv")[0].textContent = `UV: ${uv}`;
     $("#todays-date")[0].textContent = `${day}, ${dayMonth} ${month}`;
-  
-    if (uv < 3){
+    if (uv < 3) {
         $("#today-uv").removeClass("moderate-uv severe-uv").addClass("favorable-uv");
-    }else if (uv < 6){
+    } else if (uv < 6) {
         $("#today-uv").removeClass("favorable-uv severe-uv").addClass("moderate-uv");
-    }else{
+    } else {
         $("#today-uv").removeClass("favorable-uv moderate-uv").addClass("severe-uv");
-    }    
+    }
 }
-
 // fetch json from API and calls functions to populate the DOM
 const getDataThenPopulatePage = (givenUrl) => {
     fetch(givenUrl)
-    .then(reponse => reponse.json())
-    .then(data => {
-        if (data == null) {
-            console.log('null data');
-        }
-        console.log('stringify: ' + JSON.stringify(data));
-        //console.log('data ' + typeof data + ', cod ' + data.list.length);
-        updateFiveDay(data.data);
-        updateToday(data.data[0]);
-        // store weather data for later use to update main card date
-        window.localStorage.setItem("weatherData", JSON.stringify(data.data));
-    })
+        .then(reponse => reponse.json())
+        .then(data => {
+            if (data == null) {
+            }
+            updateFiveDay(data.data);
+            updateToday(data.data[0]);
+            // store weather data for later use to update main card date
+            window.localStorage.setItem("weatherData", JSON.stringify(data.data));
+        })
 }
-
-// create event listerner for the search button
-searchBtn.click(event => { 
+// create event listener for the search button
+searchBtn.click(event => {
     event.preventDefault();
-   
-    // Ensure correct formatting of search string
+    // ensure correct formatting of search string
     let tmpCityName = $("#search-btn").siblings("input")[0].value;
-
-    // Checks for a input with atleast one none-whitespace char
-    if(tmpCityName.trim()){
+    // check for an input with at least one none-whitespace char
+    if (tmpCityName.trim()) {
         tmpCityName = tmpCityName.trim();
         tmpCityName = tmpCityName.toLowerCase();
         cityName = tmpCityName[0].toUpperCase() + tmpCityName.slice(1);
         getCityData(getEndPointCity(cityName));
     }
-
 });
-
-// fetch json from API and calls functions to populate the DOM
+// fetch json from API and call functions to populate the DOM
 const getCityData = (givenUrl) => {
     fetch(givenUrl)
-    .then(reponse => reponse.json())
-    .then(data => {
-        console.log(data);
-        searchCity(data);
-    })
+        .then(reponse => reponse.json())
+        .then(data => {
+            searchCity(data);
+        })
 }
-var lastCityStr = window.localStorage.getItem("lastCity");
-var cityName = '';
-if (lastCityStr != null) {
-    console.log('last: ' + lastCityStr);
-    var lastCity = JSON.parse(lastCityStr);
-    console.log('last city: ' + JSON.stringify(lastCity));
-    //cityName = lastCity.cityName;
-    cityFullPath = lastCity.cityName;
-    // Handle optional city state
-    if (lastCity.cityState && lastCity.cityState != 'undefined') {
-        cityFullPath += ', ' + lastCity.cityState;
+// load previously searched weather and cities
+const initPreviouslySearched = () => {
+    var lastCityStr = window.localStorage.getItem("lastCity");
+    if (lastCityStr != null) {
+        var lastCity = JSON.parse(lastCityStr);
+        // cityName = lastCity.cityName;
+        cityFullPath = lastCity.cityName;
+        // handle optional city state
+        if (lastCity.cityState && lastCity.cityState != 'undefined') {
+            cityFullPath += ', ' + lastCity.cityState;
+        }
+        cityFullPath += ', ' + lastCity.cityCountry;
+        getDataThenPopulatePage(getEndPoint(lastCity.cityLat, lastCity.cityLon));
+        populatePreviouslySearched();
+    } else {
+        $('#weather-display').hide();
     }
-    cityFullPath += ', ' + lastCity.cityCountry;
-    getDataThenPopulatePage(getEndPoint(lastCity.cityLat, lastCity.cityLon));
-    populatePreviouslySearched();
-} else {
-    $('#weather-display').hide();
 }
+initPreviouslySearched();
 
 $("#clear-history").on("click", clearPreviouslySearched);
-//$(".daily-card").on("click", updateMainDate);
